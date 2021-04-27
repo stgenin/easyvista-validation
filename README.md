ca54b07a5fd7419a2800b4f72056bfd2d8156a25
Stéphane Génin <stephane.genin@smartview.fr>
2021-04-27 16:25:58 +0200

    Rename the launcher. It must end with Test.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Créer le lanceur (rest/RunCucumberForRest)
+Créer le lanceur (rest/RunCucumberForRestTest)
 Ajouter les dépendances : io.cucumber/cucumber-java, io.cucumber/cucumber-junit
 Créer le fichier feature
 Lancer le lanceur
--- src/test/java/RunCucumberForRestTest.java
+++ src/test/java/RunCucumberForRestTest.java
@@ -1,11 +1,9 @@
-package rest;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/rest",strict = true, monochrome = true, plugin = {"pretty","json:target/cucumber.json"})
-public class RunCucumberForRest {
+public class RunCucumberForRestTest {
 
 }
