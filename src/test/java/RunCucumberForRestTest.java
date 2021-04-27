f85723aaec6e53d100b2c101ab55baabd733f464
Stéphane Génin <stephane.genin@smartview.fr>
2021-04-27 16:54:35 +0200

    Fix the feature path

--- src/test/java/RunCucumberForRestTest.java
+++ src/test/java/RunCucumberForRestTest.java
@@ -3,7 +3,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/rest",strict = true, monochrome = true, plugin = {"pretty","json:target/cucumber.json"})
+@CucumberOptions(features="src/test/resources",strict = true, monochrome = true, plugin = {"pretty","json:target/cucumber.json"})
 public class RunCucumberForRestTest {
 
 }
